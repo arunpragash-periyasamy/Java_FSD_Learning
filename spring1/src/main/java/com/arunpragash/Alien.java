53832bc100e2e527268ffaf718477d1c855e257c
arunpragash-periyasamy <arunpragash46@gmail.com>
2024-06-20 21:05:49 +0530

    Created a new maven project for developing a spring application without spring boot. Now I have created a basic class and method inside the class. Tested the method of the Alien object it works fine. And then added the dependency of the spring content

---
+++ spring1/src/main/java/com/arunpragash/Alien.java
@@ -0,0 +1,7 @@
+package com.arunpragash;
+
+public class Alien {
+    public void code() {
+        System.out.println("Coding....");
+    }
+}
---
+++ spring1/src/main/java/com/arunpragash/App.java
@@ -0,0 +1,15 @@
+package com.arunpragash;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+        Alien alien = new Alien();
+        alien.code();
+    }
+}
