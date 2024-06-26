853894c5458989a1910eb8edb222ef372e386397
arunpragash-periyasamy <arunpragash46@gmail.com>
2024-06-26 17:30:10 +0530

    created a form in jsp and handling a request to get the data from the form and send a result with another jsp page using servlet and http session

--- SpringBootWeb1/src/main/java/com/arunpragash/SpringBootWeb1/HelloController.java
+++ SpringBootWeb1/src/main/java/com/arunpragash/SpringBootWeb1/HelloController.java
@@ -3,10 +3,23 @@ package com.arunpragash.SpringBootWeb1;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class HelloController {
     @RequestMapping("/")
     public String home() {
         return "index.jsp";
+    }
+
+    @RequestMapping("/add")
+    public String add(HttpServletRequest req, HttpSession session) {
+        int num1 = Integer.parseInt(req.getParameter("num1"));
+        int num2 = Integer.parseInt(req.getParameter("num2"));
+        int result = num1 + num2;
+        System.out.println(result);
+        session.setAttribute("result", result);
+        return "result.jsp";
     }
 }
--- SpringBootWeb1/src/main/webapp/index.jsp
+++ SpringBootWeb1/src/main/webapp/index.jsp
@@ -1,6 +1,10 @@
 <%@page language="java" %>
 <html>
     <body>
-        <h1>My First JSP Page</h1>
+        <form action="add">
+            <input type="text" name="num1">
+            <input type="text" name="num2">
+            <button type="submit">Submit</button>
+        </form>
     </body>
 </html>
