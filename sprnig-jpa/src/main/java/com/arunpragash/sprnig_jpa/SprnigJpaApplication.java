3fb2bf4e0cbbc32196371716c91732bae7d32f87
arunpragash-periyasamy <arunpragash46@gmail.com>
2024-07-04 12:01:10 +0530

    Tried to insert and get the data by searching with specific column with JPQL and dynamic finding methods. Updating and deleting the records in the table

--- sprnig-jpa/src/main/java/com/arunpragash/sprnig_jpa/SprnigJpaApplication.java
+++ sprnig-jpa/src/main/java/com/arunpragash/sprnig_jpa/SprnigJpaApplication.java
@@ -16,7 +16,6 @@ public class SprnigJpaApplication {
 		StudentRepo repo = context.getBean(StudentRepo.class);
 
 		Student s1 = context.getBean(Student.class);
-		s1.setId(1);
 		s1.setRollno(101);
 		s1.setName("Arun");
 		s1.setMarks(80.5f);
@@ -27,7 +26,49 @@ public class SprnigJpaApplication {
 		s2.setName("Pragash");
 		s2.setMarks(90.5f);
 		
-		repo.save(s1);
+		Student s3 = context.getBean(Student.class);
+		s3.setId(3);
+		s3.setRollno(103);
+		s3.setName("Raghav");
+		s3.setMarks(70.5f);
+
+		// repo.save(s1);
+		// repo.save(s2);
+		// repo.save(s3);
+		
+
+		// repo.findAll().forEach(System.out::println);
+
+		// Student s4 = repo.findById(1).get();
+
+		// System.out.println(repo.findByName("Arun"));
+		// System.out.println("\n\n\n");
+		// System.out.println(repo.findByMarksGreaterThan(70.0f));
+		// System.out.println("\n\n\n");
+
+		s3.setRollno(105);
+		s3.setName("Raghav");
+		s3.setMarks(70.5f);
+		//  updating the data 
+		
+		repo.save(s3);
+
+
+
+
+		Student s5 = context.getBean(Student.class);
+		s5.setId(5);
+		s5.setRollno(102);
+		s5.setName("Kanishkar");
+		s5.setMarks(90.5f);
+
+		repo.save(s5);
+		repo.findAll().forEach(System.out::println);
+
+		repo.delete(s5);
+
+
+		repo.findAll().forEach(System.out::println);
 	}
 
 }
